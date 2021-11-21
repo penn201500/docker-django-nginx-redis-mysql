50d92c6dc19fc1cecaa3d4a3f9d0fb77dea1c0ed
ubuntu <ubuntu@localhost.localdomain>
2021-11-22 00:14:25 +0800

    create superuser only needed at first startup

--- myproject/start.sh
+++ myproject/start.sh
@@ -15,11 +15,11 @@ python manage.py collectstatic --noinput&&
 python manage.py makemigrations&&
 python manage.py migrate&&
 python manage.py createcachetable&&
-python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL \
-&&
+#python manage.py createsuperuser \    # only needed at first startup
+#        --noinput \
+#        --username $DJANGO_SUPERUSER_USERNAME \
+#        --email $DJANGO_SUPERUSER_EMAIL \
+#&&
 uwsgi --ini /var/www/html/myproject/uwsgi.ini&&
 tail -f /dev/null
 
